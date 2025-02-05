4803bb830bf30c4b4e214536a6a8660a3751e349
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-02-04 21:47:51 -0800

    apparently it works now.

--- server/src/routes/api/ticketmasterRoutes.ts
+++ server/src/routes/api/ticketmasterRoutes.ts
@@ -7,13 +7,10 @@ const router = Router();
 
 router.get('/', async (req: Request, res:Response) => {
     try{
-        //test API call
-        //const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=30.280735&lon=-97.730837&appid=de2ad693a9acf83a5d656679207303d5");
-
         const { city, stateCode } = req.body;
         console.log(`city = ${city}, stateCode = ${stateCode}`);
-        console.log(`${process.env.EVENT_API_BASE_URL}events.json?city=${city}&stateCode=${stateCode}&apikey=${process.env.EVENT_API_KEY}`)
-        const response = await fetch(`${process.env.EVENT_API_BASE_URL}events.json?city=${city}&stateCode=${stateCode}&apikey=${process.env.EVENT_API_KEY}`);
+        //console.log(`${process.env.EVENT_API_BASE_URL}events.json?city=${city}&stateCode=${stateCode}&apikey=${process.env.EVENT_API_KEY}`)
+        const response = await fetch(`${process.env.EVENT_API_BASE_URL}events.json?stateCode=${stateCode}&city=${city}&apikey=${process.env.EVENT_API_KEY}`);
 
         console.log(` \n API call returned with status: ${response.status}: ${response.statusText}`);
 
