1ef5b0c6142b11d76599a5f72835feae24213253
EnderJunk <christian.walters1996@gmail.com>
2025-02-06 19:30:11 -0700

    frontend framework + a little polish

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,20 +1,24 @@
-const getWeather = async (latitude: string, longitude: string) => {
-  try {
-    const response = await fetch(
-      `/api/index?lat=${latitude}&lon=${longitude}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ latitude, longitude }),
-      }
-    );
+// import { Coordinates } from "../interfaces/coordinates";
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log("error");
-  }
-};
+// const getWeather = async (latitude: string, longitude: string) => {
+//   try {
+//     const response = await fetch(
+//       `/api/weatherRoutes?lat=${latitude}&lon=${longitude}`,
+//       {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ latitude, longitude }),
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (err) {
+//     console.log("error");
+//   }
+// };
+
+// https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={apikey}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
